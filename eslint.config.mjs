75e52485ecbbbff33f2c8d0dd75918cdabdd668c
pgirald <pgirald18@gmail.com>
2026-05-22 11:45:06 -0500

    update eslint configuration to disable no-document-import-in-page rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,9 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: { "@next/next/no-document-import-in-page": "off" },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
